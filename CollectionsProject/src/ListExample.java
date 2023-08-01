6f3ed0a4039584a057844b09d525a68c0d482af1
Vilas Sangre <vilas.sangre@gmail.com>
2023-08-01 09:29:56 +0530

    First Commit

---
+++ CollectionsProject/src/ListExample.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeSet;
+
+public class ListExample {
+
+	public static void main(String[] args) {
+		int count = 0;
+		do{
+		  System.out.println("Interviewbit");
+		  count++;
+		} while(count < 10);
+		
+		
+
+	}
+
+}
+
+
+
+//Joint Account(Husband-Balance ,Wife->15000)->ATM Card->
